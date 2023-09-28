3a5b9aff69764c45b215d65cd7a0e35f3fdbfd55
pcrainer38 <pammie_78704@yahoo.com>
2023-09-28 14:48:28 -0500

    update js

--- script.js
+++ script.js
@@ -16,19 +16,19 @@ generateBtn.addEventListener("click", writePassword);
 var numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var upCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q","r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var spCharArr = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '\',  '^', '_', '`', '{', '|', '}', '~'];
+// var specialCharArr = ['!','#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '\',  '^', '_', '`', '{', '|', '}', '~'];
 
 var characters = [];
 var newPassword = "";
 
-function generatePassword (length, inclNum, upperCase, lowerCase) {
+function generatePassword () {
     var numChar = window.prompt("How many characters would your password to be?", "Between 8 and 128");
     console.log(numChar);
-    var inclNum = window.prompt ("Would you like to include numbers?", "yes or no");
+    var inclNum = window.confirm ("Would you like to include numbers?", "yes or no");
     console.log(inclNum);
-    var upperCase = window.prompt ("Would you like to include uppercase letters?", "Yes or no");
+    var upperCase = window.confirm ("Would you like to include uppercase letters?", "Yes or no");
     console.log(upperCase);
-    var lowerCase = window.prompt ("Would you like to include lower case letters?", "yes or no");
+    var lowerCase = window.confirm ("Would you like to include lower case letters?", "yes or no");
     console.log(lowerCase);
     // var symbols = window.prompt ("Would you like to include special characters?", "yes or no");
     
@@ -42,6 +42,10 @@ function generatePassword (length, inclNum, upperCase, lowerCase) {
       var characters = lowCaseArr.concat(characters);
     } 
 
+    // if (symbols) {
+    //   var characters = specialCharArr.concat(characters);
+    // }
+
     console.log(characters);
     
 
@@ -52,13 +56,11 @@ function generatePassword (length, inclNum, upperCase, lowerCase) {
 
 return (newPassword);
 
-    }
+}
     
-  
+writePassword();
 
-writePassword()
-
-// generatePassword();
+generatePassword();
 
 
 
