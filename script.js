9bbbc05843d6862994817e92751fadfcd1820ce9
pcrainer38 <pammie_78704@yahoo.com>
2023-09-28 19:21:47 -0500

    fixed errors in js

--- script.js
+++ script.js
@@ -13,16 +13,29 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 var numArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var upCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q","r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var specialCharArr = ['!','#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '\',  '^', '_', '`', '{', '|', '}', '~'];
+var specialCharArr = ['!','#', '$', '%', '&', '(', ')', '*', '+', '-', ';', ':', '@']
+
 
 var characters = [];
 var newPassword = "";
 
 function generatePassword () {
-    var numChar = window.prompt("How many characters would your password to be?", "Between 8 and 128");
+    var numChar = parseInt(window.prompt("How many characters would your password to be?", "Between 8 and 128"));
+
+    if (Number.isNaN(numChar)) {
+      alert("Length has to be a number. Please, retry.");
+      return("Length has to be a number. Please, retry.");
+    } 
+
+    if(numChar < 8 || numChar > 128) {
+      alert("Length has to be a number between 8 and 128. Please, retry.");
+      return("Length has to be a number between 8 and 128. Please, retry.");
+    }
+
     console.log(numChar);
     var inclNum = window.confirm ("Would you like to include numbers?", "yes or no");
     console.log(inclNum);
@@ -30,37 +43,40 @@ function generatePassword () {
     console.log(upperCase);
     var lowerCase = window.confirm ("Would you like to include lower case letters?", "yes or no");
     console.log(lowerCase);
-    // var symbols = window.prompt ("Would you like to include special characters?", "yes or no");
-    
-    if (inclNum) {
-      var characters = numArr.concat(characters);
-    }
-    if (upperCase) {
-      var characters = upCaseArr.concat(characters);
-    }
-    if (lowerCase) {
-      var characters = lowCaseArr.concat(characters);
-    } 
+    var symbols = window.confirm ("Would you like to include special characters?", "yes or no");
+ 
 
-    // if (symbols) {
-    //   var characters = specialCharArr.concat(characters);
-    // }
+    if (inclNum) {
+       characters = numArr.concat(characters);
+    } 
+    console.log(characters);
+    if (upperCase) {
+       characters = upCaseArr.concat(characters);
+    }
+    console.log(characters);
+    if (lowerCase) {
+      characters = lowCaseArr.concat(characters);
+    } 
+    console.log(characters);
+
+    if (symbols) {
+     characters = specialCharArr.concat(characters);
+    }
 
     console.log(characters);
     
 
-    for(i = 0; i < numChar.length; i++) {
-      // console.log(numChar);
-      newPassword = characters[i][Math.floor(Math.random() * numChar.length)];
+    for(i = 0; i < numChar; i++) {
+
+      newPassword += characters[Math.floor(Math.random() * characters.length)];
     }
 
-return (newPassword);
+  return (newPassword);
 
 }
-    
-writePassword();
 
-generatePassword();
+
+
 
 
 
